dc63695120c7f0c7cc9a9288aee1716c50b0270f
shishir-sap <shishirsapkota@msn.com>
2022-12-09 11:38:31 +0000

    webex chat bot

--- bot.py
+++ bot.py
@@ -1,4 +1,9 @@
 import os
 from webex_bot.webex_bot import WebexBot
+
+#retrive the api token that we export
+# to export the token use export WEBEX_TOKEN= "Put the token here"
+
 webex_token = os.environ["WEBEX_TOKEN"]
 bot = WebexBot(webex_token)
+
